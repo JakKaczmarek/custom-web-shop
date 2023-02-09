c0a749cc91c127eaf6680c323fe1126011ac7dad
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-02-09 17:12:06 +0100

    Created new repository web shop

---
+++ custom-web-shop/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Custom web shop</h1>
+        <p>test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
