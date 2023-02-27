859538b65b0ae98fe94a2df7dbf198822a80293d
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-02-27 18:00:21 +0100

    Added new bike photos

--- src/mocks/bikes.jsx
+++ src/mocks/bikes.jsx
@@ -1,4 +1,17 @@
-import { bike1, bike2, bike3, bike4, bike5 } from "./bikesPng";
+import {
+  bike1,
+  bike2,
+  bike3,
+  bike4,
+  bike5,
+  bike6,
+  bike7,
+  bike8,
+  bike9,
+  bike10,
+  bike11,
+  bike12,
+} from "./bikesPng";
 
 export const bikes = [
   { src: bike1, alt: "bike1", id: 1 },
@@ -6,4 +19,11 @@ export const bikes = [
   { src: bike3, alt: "bike3", id: 3 },
   { src: bike4, alt: "bike4", id: 4 },
   { src: bike5, alt: "bike5", id: 5 },
+  { src: bike6, alt: "bike6", id: 6 },
+  { src: bike7, alt: "bike7", id: 7 },
+  { src: bike8, alt: "bike8", id: 8 },
+  { src: bike9, alt: "bike9", id: 9 },
+  { src: bike10, alt: "bike10", id: 10 },
+  { src: bike11, alt: "bike11", id: 11 },
+  { src: bike12, alt: "bike12", id: 12 },
 ];
