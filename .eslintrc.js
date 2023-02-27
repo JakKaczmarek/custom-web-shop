db6c76bcb40dd46e26b648003287b55c4b6de42b
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-02-27 18:06:52 +0100

    Added new eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ["react", "prettier"],
   rules: {
+    "no-alert": "off",
     "react/react-in-jsx-scope": 0,
     "linebreak-style": ["error", "windows"],
     "import/prefer-default-export": 0,
