b5162de8b0bb05e5f2ef1d4d18a93907fb2c0516
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-03-14 16:27:06 +0100

    Added new folders with photos of bikes

--- src/mocks/bikesPng/index.js
+++ src/mocks/bikesPng/index.js
@@ -1,12 +1,59 @@
-export { default as bike1 } from "./bike1.jpg";
-export { default as bike2 } from "./bike2.jpg";
-export { default as bike3 } from "./bike3.jpg";
-export { default as bike4 } from "./bike4.jpg";
-export { default as bike5 } from "./bike5.jpg";
-export { default as bike6 } from "./bike6.jpg";
-export { default as bike7 } from "./bike7.jpg";
-export { default as bike8 } from "./bike8.jpg";
-export { default as bike9 } from "./bike9.jpg";
-export { default as bike10 } from "./bike10.jpg";
-export { default as bike11 } from "./bike11.jpg";
-export { default as bike12 } from "./bike12.jpg";
+export { default as bike1 } from "./bike1/bike1.jpg";
+export { default as bike1f1 } from "./bike1/bike11.jpg";
+export { default as bike1f2 } from "./bike1/bike12.jpg";
+export { default as bike1f3 } from "./bike1/bike13.jpg";
+
+export { default as bike2 } from "./bike2/bike2.jpg";
+export { default as bike2f1 } from "./bike2/bike21.jpg";
+export { default as bike2f2 } from "./bike2/bike22.jpg";
+export { default as bike2f3 } from "./bike2/bike23.jpg";
+
+export { default as bike3 } from "./bike3/bike3.jpg";
+export { default as bike3f1 } from "./bike3/bike31.jpg";
+export { default as bike3f2 } from "./bike3/bike32.jpg";
+export { default as bike3f3 } from "./bike3/bike33.jpg";
+
+export { default as bike4 } from "./bike4/bike4.jpg";
+export { default as bike4f1 } from "./bike4/bike41.jpg";
+export { default as bike4f2 } from "./bike4/bike42.jpg";
+export { default as bike4f3 } from "./bike4/bike43.jpg";
+
+export { default as bike5 } from "./bike5/bike5.jpg";
+export { default as bike5f1 } from "./bike5/bike51.jpg";
+export { default as bike5f2 } from "./bike5/bike52.jpg";
+export { default as bike5f3 } from "./bike5/bike53.jpg";
+
+export { default as bike6 } from "./bike6/bike6.jpg";
+export { default as bike6f1 } from "./bike6/bike61.jpg";
+export { default as bike6f2 } from "./bike6/bike62.jpg";
+export { default as bike6f3 } from "./bike6/bike63.jpg";
+
+export { default as bike7 } from "./bike7/bike7.jpg";
+export { default as bike7f1 } from "./bike7/bike71.jpg";
+export { default as bike7f2 } from "./bike7/bike72.jpg";
+export { default as bike7f3 } from "./bike7/bike73.jpg";
+
+export { default as bike8 } from "./bike8/bike8.jpg";
+export { default as bike8f1 } from "./bike8/bike81.jpg";
+export { default as bike8f2 } from "./bike8/bike82.jpg";
+export { default as bike8f3 } from "./bike8/bike83.jpg";
+
+export { default as bike9 } from "./bike9/bike9.jpg";
+export { default as bike9f1 } from "./bike9/bike91.jpg";
+export { default as bike9f2 } from "./bike9/bike92.jpg";
+export { default as bike9f3 } from "./bike9/bike93.jpg";
+
+export { default as bike10 } from "./bike10/bike10.jpg";
+export { default as bike10f1 } from "./bike10/bike101.jpg";
+export { default as bike10f2 } from "./bike10/bike102.jpg";
+export { default as bike10f3 } from "./bike10/bike103.jpg";
+
+export { default as bike11 } from "./bike11/bike11.jpg";
+export { default as bike11f1 } from "./bike11/bike11.jpg";
+export { default as bike11f2 } from "./bike11/bike112.jpg";
+export { default as bike11f3 } from "./bike11/bike113.jpg";
+
+export { default as bike12 } from "./bike12/bike12.jpg";
+export { default as bike12f1 } from "./bike12/bike121.jpg";
+export { default as bike12f2 } from "./bike12/bike122.jpg";
+export { default as bike12f3 } from "./bike12/bike123.jpg";
