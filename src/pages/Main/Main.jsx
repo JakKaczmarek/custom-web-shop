b345f0f360645c63eb68229ca79bfa1fd815b30b
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-03-16 17:06:26 +0100

    Added footer

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import Products from "../../components/Products/Products";
+import Footer from "../../components/Footer/Footer";
 
 export default function Main() {
   return (
     <>
       <NavBar />
       <Products />
+      <Footer />
     </>
   );
 }
