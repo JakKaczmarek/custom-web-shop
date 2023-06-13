28fc2c920712854f7a9467f6b82d6a65b3420ff5
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-06-13 16:06:53 +0200

    Added temporary pagination with MUI buttons

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import { IData } from "../../../@types/types";
 
 export function Pagination(
@@ -12,49 +14,115 @@ export function Pagination(
         &nbsp;
         <div className="pagination">{page}</div>
         &nbsp;
-        <button
-          className="pagination"
-          type="button"
-          onClick={() => callback(page + 1)}
-        >
-          NEXT
-        </button>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="contained"
+            sx={{
+              background: "black",
+              color: "white",
+              ":hover": {
+                bgcolor: "grey",
+              },
+            }}
+            onClick={() => callback(page + 1)}
+          >
+            Next
+          </Button>
+        </Stack>
       </div>
     );
   }
-  if (data!.length < 3) {
+  if (data!.length < 3 && data!.length > 0) {
     return (
       <div className="pagination">
-        <button
-          className="pagination"
-          type="button"
-          onClick={() => callback(page - 1)}
-        >
-          PREV
-        </button>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="contained"
+            sx={{
+              background: "black",
+              color: "white",
+              ":hover": {
+                bgcolor: "grey",
+              },
+            }}
+            onClick={() => callback(page - 1)}
+          >
+            Prev
+          </Button>
+        </Stack>
         <div className="pagination">{page}</div>
       </div>
     );
   }
+  if (data?.length)
+    return (
+      <div className="pagination">
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="contained"
+            sx={{
+              background: "black",
+              color: "white",
+              ":hover": {
+                bgcolor: "grey",
+              },
+            }}
+            onClick={() => callback(page - 1)}
+          >
+            Prev
+          </Button>
+        </Stack>
+        &nbsp;
+        <div className="pagination">{page}</div>
+        &nbsp;
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="contained"
+            sx={{
+              background: "black",
+              color: "white",
+              ":hover": {
+                bgcolor: "grey",
+              },
+            }}
+            onClick={() => callback(page + 1)}
+          >
+            Next
+          </Button>
+        </Stack>
+      </div>
+    );
   return (
-    <div className="pagination">
-      <button
-        className="pagination"
-        type="button"
-        onClick={() => callback(page - 1)}
-      >
-        PREV
-      </button>
+    <div>
+      <div>
+        <h2 className="pagination">More bikes later...</h2>
+      </div>
       &nbsp;
-      <div className="pagination">{page}</div>
+      <div>
+        <Stack
+          direction="column"
+          spacing={1}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button
+            variant="contained"
+            sx={{
+              height: 75,
+              width: 200,
+              background: "white",
+              color: "black",
+              ":hover": {
+                bgcolor: "grey",
+              },
+            }}
+            onClick={() => callback(page - 1)}
+          >
+            Back to bikes
+          </Button>
+        </Stack>
+      </div>
       &nbsp;
-      <button
-        className="pagination"
-        type="button"
-        onClick={() => callback(page + 1)}
-      >
-        NEXT
-      </button>
     </div>
   );
 }
