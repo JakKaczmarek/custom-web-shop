7128cc176fc55c301da9b58e53e8c64ee39153c6
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-07-20 10:22:57 +0200

    Added button in CustomerOrders

--- src/pages/Customer/CustomerOrders.tsx
+++ src/pages/Customer/CustomerOrders.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CustomerOrderTable from "./CustomerOrderTable";
 import { IOrders } from "../../../@types/types";
 import { loadData } from "../../img/FetchData";
@@ -7,6 +8,11 @@ import { AuthContext } from "../../contexts/AuthContext";
 export default function CustomerOrders() {
   const [orderData, setOrderData] = useState<IOrders[] | null>(null);
   const { userEmail } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleBackToShop = () => {
+    navigate("/");
+  };
 
   useEffect(() => {
     loadData(
@@ -16,7 +22,17 @@ export default function CustomerOrders() {
   });
   return (
     <div>
-      <p className="CustomerHelloHistory">Welcome {userEmail}!</p>
+      <div className="CustomerLeft">
+        <button
+          type="button"
+          className="CustomerBackBtn"
+          onClick={handleBackToShop}
+        >
+          Back to shop
+        </button>
+        <p className="CustomerHelloHistory">Welcome {userEmail}!</p>
+      </div>
+
       <div className="CustomerTable">
         <h2 className="CustomerTitleHistory">History</h2>
         <div className="AdminTable">{CustomerOrderTable(orderData)}</div>
