0b5c6abcbf82a9edbea48913ec1a9db48a7f5da8
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-08-02 11:55:38 +0200

    Fixed problem in config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -7,12 +7,11 @@ module.exports = function override(config) {
   Object.assign(fallback, {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
+    os: require.resolve("os-browserify/browser"),
     assert: require.resolve("assert"),
-    http: require.resolve("stream-http"),
-    https: require.resolve("https-browserify"),
-    os: require.resolve("os-browserify"),
     url: require.resolve("url"),
     path: require.resolve("path-browserify"),
+    buffer: require.resolve("buffer"),
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
